bd7f5f167b73dd8d142fbe7e45c1b7ed4cc5c1d2
Aidan Abbott <aidan.abbott@student.manchester.ac.uk>
2017-01-26 18:12:45 +0000

    Missing files commited, and contact Me Section Filled
    Changed to Contact me component:
    - Added a minimalist map
    - Added contact details for email, skype, linkedin, github, instagram & twitter
    
    Commited basic application files

---
+++ src/app/cv/cv.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CvComponent } from './cv.component';
+
+describe('Component: Cv', () => {
+  it('should create an instance', () => {
+    let component = new CvComponent();
+    expect(component).toBeTruthy();
+  });
+});
